4a1ee3ee03fe6826f5903ff5f6805684e9e55bc8
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-17 20:50:21 +0300

    feat: adding fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-
-
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import useFont from "./hooks/useFont";
+import { useEffect } from "react";
+import { SplashScreen } from "expo-router";
 
 export default function App() {
+  const { fontsLoaded, error } = useFont();
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return;
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-emerald-300">Index</Text>
+      <Text style={{ fontFamily: "Poppins-Black" }} className="text-3xl ">
+        Aora!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
