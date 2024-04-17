e08d5a74660171d2231801b6e11a4b5d0f79ea8b
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-17 19:57:56 +0300

    fix: type errors on classnames

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+
+
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1  items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-emerald-300">Index</Text>
       <StatusBar style="auto" />
     </View>
