d72d86040b73c46dcb84d5eb210546995a86a26d
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-18 10:58:37 +0300

    feat: adding colors to the taiwlind config

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import useFont from "./hooks/useFont";
-import { useEffect } from "react";
 import { Link, SplashScreen } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import useFont from "./hooks/useFont";
 
 export default function App() {
   const { fontsLoaded, error } = useFont();
@@ -15,12 +17,9 @@ export default function App() {
 
   if (!fontsLoaded && !error) return;
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text style={{ fontFamily: "Poppins-Black" }} className="text-3xl ">
-        Aora!
-      </Text>
-      <Link href="/home">Go home</Link>
-      <StatusBar style="auto" />
-    </View>
+    // Ensure that content does not overlap
+    <SafeAreaView className="bg-primary h-full">
+
+    </SafeAreaView>
   );
 }
