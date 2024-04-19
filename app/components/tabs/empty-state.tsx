8c33427ab2f4211d5048e22e93801ab0f4a55f46
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-20 01:57:23 +0300

    feat: empty screen

--- app/components/tabs/empty-state.tsx
+++ app/components/tabs/empty-state.tsx
@@ -1,5 +1,6 @@
-import { View, Text, Image } from "react-native";
 import React from "react";
+import { Image, Text, View } from "react-native";
+
 import images from "../../../constants/images";
 
 type EmptyStateProps = {
@@ -14,6 +15,11 @@ const EmptyState = ({ title, description }: EmptyStateProps) => {
         resizeMode="contain"
         className="w-[270px] h-[215px] "
       />
+
+      <Text className="text-gray-100 text-sm font-medium"> {description} </Text>
+      <Text className="text-white mt-2  font-semibold text-xl text-center">
+        {title}
+      </Text>
     </View>
   );
 };
