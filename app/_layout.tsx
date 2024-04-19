4b9cf74fb2f2aeef364479ffe442d25bd2b6a728
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-19 16:55:44 +0300

    feat: context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Slot, Stack } from "expo-router";
-
+import GlobalProvider from '../context/GlobalProvide'
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="/ser" options={{ headerShown: false }} /> */}
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="/ser" options={{ headerShown: false }} /> */}
+      </Stack>
+    </GlobalProvider>
   );
 };
 
