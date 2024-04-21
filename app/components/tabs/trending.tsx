c8a78f8571238fc0c8daa1871ede9926756acf06
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-21 20:23:18 +0300

    feat: trending videos

--- app/components/tabs/trending.tsx
+++ app/components/tabs/trending.tsx
@@ -1,16 +1,80 @@
-import { Text, FlatList } from "react-native";
-import React from "react";
+import {
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
 
+import * as Animatable from "react-native-animatable";
+import icons from "../../../constants/icons";
 type TrendingProps = {
   posts: any;
 };
+
+type TrendingItemProps = {
+  activeItem: any;
+  item: any;
+};
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+function TrendingItem({ activeItem, item }: TrendingItemProps) {
+  const [play, setPlay] = useState(false);
+
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          onPress={() => setPlay(true)}
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5  overflow-hidden shadow-lg shadow-black/40"
+          />
+
+          <Image
+            source={icons.play}
+            resizeMode="contain"
+            className="w-12 h-12 absolute"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+}
 const Trending = ({ posts }: TrendingProps) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
+        <TrendingItem item={item} activeItem={activeItem} />
       )}
       horizontal
     />
