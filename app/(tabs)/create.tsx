f05992ff748c6de26b1df798280fed583a6de7cf
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-23 08:25:32 +0300

    feat: create page

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,6 +1,8 @@
+import { ResizeMode, Video } from "expo-av";
 import React, { useState } from "react";
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
+import icons from "../../constants/icons";
 import FormField from "../components/auth/form-field";
 
 const Create = () => {
@@ -25,6 +27,55 @@ const Create = () => {
           title="Video Title"
           otherStyles="mt-10"
         />
+        <View className="mt-7 space-y-2">
+          <Text className="text-gray-100 text-base  font-semibold">
+            Upload video
+          </Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-black rounded-2xl justify-center items-center">
+                <View className="border border-dashed border-secondary justify-center items-center w-14 h-14">
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    className="w-5 h-5"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <View className="mt-7 space-y-2">
+          <Text className="text-gray-100 text-base  font-semibold">
+            Thumbnail image
+          </Text>
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail }}
+                className="w-full h-64 rounded-2xl"
+                resizeMode="cover"
+              />
+            ) : (
+              <View className="w-full h-16 border-black flex-row space-x-2 px-4 bg-black rounded-2xl justify-center items-center">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  className="w-1/2 h-1/2"
+                />
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
