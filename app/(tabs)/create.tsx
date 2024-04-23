987b21a160a9dc532f18eec8abd0596d2d8d2903
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-23 08:15:42 +0300

    feat: create page

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+
+import FormField from "../components/auth/form-field";
 
 const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-white text-2xl font-semibold">Upload video</Text>
 
-export default Create
+        <FormField
+          value={form.title}
+          placeholder="Give your video a catch title"
+          handleChangeText={(e) => {
+            setForm({ ...form, title: e });
+          }}
+          title="Video Title"
+          otherStyles="mt-10"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Create;
